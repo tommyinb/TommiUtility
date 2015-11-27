f3ea4780cdcb340972b7da9985c890184ffcae6a
tommyinb <tommyinb@yahoo.com.hk>
2015-11-27 09:53:12 +0800

    Rename LazyUtil.CreateValue to LazyUtil.EnsureInitialized

--- TommiUtility/ProgramFlow/LazyUtil.cs
+++ TommiUtility/ProgramFlow/LazyUtil.cs
@@ -2,15 +2,33 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace TommiUtility.ProgramFlow
 {
     public static class LazyUtil
     {
-        public static void CreateValue<T>(this Lazy<T> lazy)
+        public static void EnsureInitialized<T>(this Lazy<T> lazy)
         {
-            new Func<T>(() => lazy.Value).Invoke();
+            var initialize = new Func<T>(() => lazy.Value);
+            initialize.Invoke();
+        }
+    }
+
+    [TestClass]
+    public class LazyUtilTest
+    {
+        [TestMethod]
+        public void TestEnsureInitialized()
+        {
+            var lazy = new Lazy<int>(() => 7);
+            Assert.IsFalse(lazy.IsValueCreated);
+
+            lazy.EnsureInitialized();
+
+            Assert.IsTrue(lazy.IsValueCreated);
+            Assert.AreEqual(7, lazy.Value);
         }
     }
 }
