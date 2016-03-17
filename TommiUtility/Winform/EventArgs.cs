ab5629eae6e83f616a396b5b6eb89db30438b8e5
tommyinb <tommyinb@yahoo.com.hk>
2016-03-17 16:15:49 +0800

    Generic EventArgs

--- TommiUtility/Winform/EventArgs.cs
+++ TommiUtility/Winform/EventArgs.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,5 +10,15 @@ namespace TommiUtility.Winform
     public class EventArgs<T> : EventArgs
     {
         public T Item { get; set; }
+    }
+
+    public static class EventArgsUtil
+    {
+        public static EventArgs<T> Create<T>(T item)
+        {
+            Contract.Ensures(Contract.Result<EventArgs<T>>() != null);
+
+            return new EventArgs<T> { Item = item };
+        }
     }
 }
