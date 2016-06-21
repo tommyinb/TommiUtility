053b969a3ff08b57e2335b2e2861f77ebf194023
tommyinb <tommyinb@yahoo.com.hk>
2016-06-21 15:10:48 +0800

    Add ConsoleHighlight

---
+++ TommiUtility/Windows/ConsoleHighlight.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TommiUtility.Windows
+{
+    public class ConsoleHighlight : IDisposable
+    {
+        public ConsoleHighlight(ConsoleColor color)
+        {
+            originalColor = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+        }
+        public void Dispose()
+        {
+            Console.ForegroundColor = originalColor;
+        }
+
+        private readonly ConsoleColor originalColor;
+
+        public static void Write(ConsoleColor color, object value)
+        {
+            using (new ConsoleHighlight(color))
+            {
+                Console.Write(value);
+            }
+        }
+        public static void WriteLine(ConsoleColor color, object value)
+        {
+            using (new ConsoleHighlight(color))
+            {
+                Console.WriteLine(value);
+            }
+        }
+    }
+}
